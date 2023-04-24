b522423c07a924989c3bf26aa25204f18384a7e7
Austin Lewis <austinlewis165@gmail.com>
2023-04-23 23:51:25 -0400

    finish css with Skye

--- public/js/conversation.js
+++ public/js/conversation.js
@@ -12,34 +12,39 @@ conversations.forEach((conversation) => {
 
 
 //***Creating a new Conversation */
+const greetingDiv = document.getElementById('greetingDiv')
 const newConvo = document.getElementById("newGroupChatBtn");
 newConvo.addEventListener("click", (e) => {
   e.preventDefault();
   //Get the parent div
-  const parentDiv = newConvo.parentNode;
+  // const parentDiv = newConvo.parentNode;
 
   //Create a holding div after parent div
-  const newGroupChatDiv = document.createElement("div");
-  parentDiv.insertAdjacentElement('afterend', newGroupChatDiv);
-
+  
+  
   //Create a new Form element
   const newForm = document.createElement("form");
   newForm.setAttribute('action', '/dashboard/newGroupChat');
+  newForm.setAttribute('class', 'newConvoForm');
   newForm.setAttribute('method', 'POST');
+  
 
   //Create a new text Input element for the group chat title
   const newInput = document.createElement("input");
   newInput.setAttribute('name', 'groupTitle');
+  newInput.setAttribute('class', 'newConvoInput');
   newInput.setAttribute('type', 'text');
   newInput.setAttribute('placeholder', 'Enter the group chat title');
 
   //Create a new submit Button Element
   const submitBtn = document.createElement("button");
+  submitBtn.setAttribute('class', 'newConvoButton');
+  submitBtn.textContent = 'Create a chatroom!';
 
   //Append everything to the form element
   newForm.appendChild(newInput);
   newForm.appendChild(submitBtn);
 
   //Append that form to the holding div
-  newGroupChatDiv.appendChild(newForm);
+  greetingDiv.insertAdjacentElement('beforeend', newForm);
 })
